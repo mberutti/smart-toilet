ca9709c1baa1f1140a7d4f75633149314e442b92
mberutti <berutt3@gmail.com>
2020-03-10 16:27:41 -0500

    initial commit

--- control.py
+++ control.py
@@ -5,3 +5,11 @@ Created on Mon Feb 24 16:27:51 2020
 @author: mberutti
 """
 
+from gpiozero import Button, LED
+
+class Control:
+    """ Class for controlling entire system
+    """
+    def __init__(self):
+        pass
+        
--- signals.py
+++ signals.py
@@ -5,3 +5,51 @@ Created on Mon Feb 24 16:05:33 2020
 @author: mberutti
 """
 
+from gpiozero import Button, LED
+
+class SignalIn:
+    """ Class to manage GPIO input signals (buttons)
+    """
+    def __init__(self, pin, **kwargs):
+        self.signal = Button(pin)
+        
+        self.led_t = None
+        self.led_f = None
+        if "pairedled" in kwargs.keys():
+            paired_pins = kwargs["pairedled"]
+            if isinstance(paired_pins, int):
+                self.led_t = LED(paired_pins)
+            elif isinstance(paired_pins, tuple):
+                self.led_t = LED(paired_pins[0])
+                self.led_f = LED(paired_pins[1])
+            else:
+                raise ValueError("Incorrect value for 'pairedled' kwarg: {}'
+                                 .format(paired_pins))
+                
+    def __exit__(self):
+        if self.led_t is not None:
+            self.led_t.off
+        if self.led_f is not None:
+            self.led_f.off
+            
+    def update(self):
+        """ Adjusts LEDS if applicable and returns signal state
+        """
+        if self.signal.is_pressed:
+            if led_t is not None:
+                self.led_t.on
+            if led_f is not None:
+                self.led_f.off        
+    
+            return True
+        
+        else:
+            if led_t is not None:
+                self.led_t.off
+            if led_f is not None:
+                self.led_f.blink
+                
+            return False
+        
+
+
